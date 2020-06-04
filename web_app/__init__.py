279d058d836ca76c66413bf0f5179e15247d52b3
Nate Dow <nate.dow@go1.com>
2020-06-03 19:53:38 -0600

    Updated with book_routes.py and books.html

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
-#from web_app.routes.book_routes import book_routes
+from web_app.routes.book_routes import book_routes
 
 # define a function to create a new flask app based on the home_routes blueprint
 def create_app():
@@ -9,7 +9,7 @@ def create_app():
     # takes the home_routes Blueprint object and registers it as an app
     # when this app is created in the code on line 16
     app.register_blueprint(home_routes)
-    #app.register_blueprint(book_routes)
+    app.register_blueprint(book_routes)
     return app
 
 if __name__ == "__main__":
