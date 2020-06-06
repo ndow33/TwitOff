804dbe192d549fb16aeb9ac5cd994978f989ed89
Nate Dow <nate.dow@go1.com>
2020-06-06 11:41:29 -0600

    Show data within db on webapp

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,25 +1,21 @@
 from flask import Blueprint, jsonify, request, render_template #, flash, redirect
 
-from web_app.models import db, Book
+from web_app.models import db, Book, parse_records
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    book_records = Book.query.all()
+    print(book_records)
+    books=parse_records(book_records)
     return jsonify(books)
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    book_records = Book.query.all()
+    print(book_records)
+    books=parse_records(book_records)
     return render_template("books.html", message="Here's some books", books=books)
 
 @book_routes.route("/books/new")
@@ -31,7 +27,7 @@ def create_book():
     print("FORM DATA:", dict(request.form))
 
     # store in database
-    new_book = Book(title=request.form["title"], author_id=request.form["author_name"])
+    new_book = Book(title=request.form["title"], author_id=request.form["author_name"]) #this line needs to match the form in new_book.html
     db.session.add(new_book)
     db.session.commit()
 
